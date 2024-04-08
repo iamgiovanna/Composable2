9561188d437ef71948d9e547a066a80ccc18e778
Giovanna Oliveira <112138845+iamgiovanna@users.noreply.github.com>
2024-04-08 20:00:55 -0300

    images

--- app/src/main/java/com/example/message/MainActivity.kt
+++ app/src/main/java/com/example/message/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.message
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,12 +14,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.message.ui.theme.MessageTheme
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.layout.ContentScale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -71,9 +77,29 @@ Column(
 @Composable
 fun BirthdayCardPreview() {
     MessageTheme {
-        GreetingText(
+        GreetingImage(
             message = " Happy Birthday Sam!",
             from = "From Emma...",
             modifier = Modifier.padding(8.dp))
     }
 }
+@Composable
+fun GreetingImage(message: String,from: String,modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.androidparty)
+   Box(modifier){
+       Image(
+        painter = image,
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        alpha = 0.5F
+    )
+       GreetingText(
+           message = message,
+           from = from,
+           modifier = Modifier
+               .fillMaxSize()
+               .padding(8.dp)
+       )
+   }
+}
+
